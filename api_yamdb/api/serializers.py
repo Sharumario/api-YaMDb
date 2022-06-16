093d34ccd6e8b441f3088a9611651bf68c6d4c99
Aleksandr Zolototrubov <azolototrubov@gmail.com>
2022-06-16 22:09:43 +0300

    First commit

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email',
+                  'first_name', 'last_name',
+                  'bio', 'role')
+        read_only_fields = ('role',)
+
+
+class SignupSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=254, required=True)
+    username = serializers.CharField(max_length=150, required=True)
+
+    class Meta:
+        fields = ('email', 'username')
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError('Использовать имя "me"'
+                                              ' запрещено')
+        return value
+
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150, required=True)
+    confirmation_code = serializers.CharField(max_length=8, required=True)
+
+    class Meta:
+        fields = ('username', 'confirmation_code')
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email',
+                  'first_name', 'last_name',
+                  'bio', 'role')
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
-
 from rest_framework import routers
 
+from api.views import UserViewSet, signup, token
+
 router_v1 = routers.SimpleRouter()
+router_v1.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
+    path('v1/auth/signup/', signup),
+    path('v1/auth/token/', token),
     path('v1/', include(router_v1.urls)),
 ]
